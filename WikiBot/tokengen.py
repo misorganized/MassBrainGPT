d1db452ec103375b46f02b575fa7cfd5d3a09fbd
David <daviru007@icloud.com>
2023-01-23 11:16:07 -0800

    token

--- WikiBot/tokengen.py
+++ WikiBot/tokengen.py
@@ -1,14 +1,11 @@
 import string
 
 # Define the characters to include in the tokens
-alphabet = string.ascii_letters + string.digits + string.punctuation + "!@#$%^&*_+-"
+alphabet = string.ascii_letters + string.digits + string.punctuation + "!@#$%^&*_+-\n "
 
-# Open the file for writing
 with open("tokens.txt", "w") as f:
-    # Generate all possible 3 letter combinations
     for c1 in alphabet:
         for c2 in alphabet:
             for c3 in alphabet:
                 token = c1 + c2 + c3
-                # Write the token to the file
                 f.write(token + "\n")
